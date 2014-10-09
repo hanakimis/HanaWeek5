3fcf404bb4d401790004bddcd3f4ad5671285f6f
Hana Kim <hanakimis@gmail.com>
2014-10-09 00:48:23 -0700

    committing up to milestone 7; 45 mins

---
+++ Tumblr/TrendingViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TrendingViewController.swift
+//  Tumblr
+//
+//  Created by Hana Kim on 10/9/14.
+//  Copyright (c) 2014 Hana. All rights reserved.
+//
+
+import UIKit
+
+class TrendingViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        scrollView.contentSize = imageView.frame.size
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
